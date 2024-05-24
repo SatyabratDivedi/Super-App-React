442bd63327eac19058e6450c7364f5cdcf59c789
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-05-24 22:58:02 +0530

    some changes

--- Super-App/client/src/components/NewsComponent.jsx
+++ Super-App/client/src/components/NewsComponent.jsx
@@ -10,36 +10,36 @@ import healthImg from "../assets/health.png";
 import politicsImg from "../assets/politics.png";
 import educationImg from "../assets/education.png";
 
-import ndtvImg from "../assets/NDTV.png";
-import favImg from "../assets/favicon.ico";
-import mintImg from "../assets/livemint.png";
-import teluguImg from "../assets/123telgu.png";
-import pinkvillaImg from "../assets/pinkvilla.png";
-import bbcImg from "../assets/bbc.png";
-import toiImg from "../assets/toi.png";
-import bLifeImg from "../assets/blife.png";
-import hTimesImg from "../assets/htimes.png";
-import quintImg from "../assets/q.png";
-import expressImg from "../assets/express.png";
-import odishaImg from "../assets/odisha.png";
-import news18Img from "../assets/news18.png";
-import moneycontrollImg from "../assets/moneycontroll.jpg";
-import theHinduImg from "../assets/theHindu.png";
-import indiaToday from "../assets/indiatoday.png";
-import abpLive from "../assets/ABP_LIVE.webp";
-import TeamBHP from "../assets/Team-BHP.png";
-import {useDispatch, useSelector} from "react-redux";
-import {decrement, increment} from "../store/pageSlice";
+// import ndtvImg from "../assets/NDTV.png";
+// import favImg from "../assets/favicon.ico";
+// import mintImg from "../assets/livemint.png";
+// import teluguImg from "../assets/123telgu.png";
+// import pinkvillaImg from "../assets/pinkvilla.png";
+// import bbcImg from "../assets/bbc.png";
+// import toiImg from "../assets/toi.png";
+// import bLifeImg from "../assets/blife.png";
+// import hTimesImg from "../assets/htimes.png";
+// import quintImg from "../assets/q.png";
+// import expressImg from "../assets/express.png";
+// import odishaImg from "../assets/odisha.png";
+// import news18Img from "../assets/news18.png";
+// import moneycontrollImg from "../assets/moneycontroll.jpg";
+// import theHinduImg from "../assets/theHindu.png";
+// import indiaToday from "../assets/indiatoday.png";
+// import abpLive from "../assets/ABP_LIVE.webp";
+// import TeamBHP from "../assets/Team-BHP.png";
+// import {useDispatch, useSelector} from "react-redux";
+// import {decrement, increment} from "../store/pageSlice";
 import toast from "react-hot-toast";
 
 const NewsComponent = () => {
   const [articles, setArticles] = useState();
-  const [articlesLength, setArticlesLength] = useState();
+  // const [articlesLength, setArticlesLength] = useState();
   const [defaultImg, setDefaultImg] = useState();
   const match = useMatch("/news/:category/:title");
   const {category} = useParams();
-  const receivePage = useSelector((state) => state.page.value);
-  const dispatch = useDispatch();
+  // const receivePage = useSelector((state) => state.page.value);
+  // const dispatch = useDispatch();
 
   const [edit, setEdit] = useState("");
 
@@ -48,13 +48,14 @@ const NewsComponent = () => {
   };
 
   const fetchNews = async () => {
-    const res = await fetch(`https://newsapi.org/v2/top-headlines?q=${edit}&country=in&category=${category}&apiKey=7e753acdd6e147029ff658f3f9f90931&page=${receivePage}`);
+    // const res = await fetch(`https://newsdata.io/api/1/news?apikey=pub_4195082b54ab7fdd21d1ef1f2710ff7a33fd1&country=in&language=en&category=${category} `);
     const data = await res.json();
-    setArticles(data.articles);
-    setArticlesLength(data.totalResults);
+    console.log(data.results);
+    setArticles(data.results);
+    // setArticlesLength(data.totalResults);
   };
 
-  const mainArticleLength = Math.ceil(articlesLength / 20);
+  // const mainArticleLength = Math.ceil(articlesLength / 20);
 
   useEffect(() => {
     fetchNews();
@@ -85,48 +86,48 @@ const NewsComponent = () => {
       default:
         return setDefaultImg(educationImg);
     }
-  }, [edit, category, receivePage]);
+  }, [category]);
 
-  const profileImg = (name) => {
-    switch (name) {
-      case "NDTV News":
-        return ndtvImg;
-      case "The Hindu":
-        return theHinduImg;
-      case "Moneycontrol":
-        return moneycontrollImg;
-      case "India Today":
-        return indiaToday;
-      case "Abplive.com":
-        return abpLive;
-      case "Team-BHP":
-        return TeamBHP;
-      case "Livemint":
-        return mintImg;
-      case "Odishatv.in":
-        return odishaImg;
-      case "The Indian Express":
-        return expressImg;
-      case "The Quint":
-        return quintImg;
-      case "Hindustan Times":
-        return hTimesImg;
-      case "Bollywood Life":
-        return bLifeImg;
-      case "The Times of India":
-        return toiImg;
-      case "123telugu.com":
-        return teluguImg;
-      case "PINKVILLA":
-        return pinkvillaImg;
-      case "BBC News":
-        return bbcImg;
-      case "News18":
-        return news18Img;
-      default:
-        return favImg;
-    }
-  };
+  // const profileImg = (name) => {
+  //   switch (name) {
+  //     case "NDTV News":
+  //       return ndtvImg;
+  //     case "The Hindu":
+  //       return theHinduImg;
+  //     case "Moneycontrol":
+  //       return moneycontrollImg;
+  //     case "India Today":
+  //       return indiaToday;
+  //     case "Abplive.com":
+  //       return abpLive;
+  //     case "Team-BHP":
+  //       return TeamBHP;
+  //     case "Livemint":
+  //       return mintImg;
+  //     case "Odishatv.in":
+  //       return odishaImg;
+  //     case "The Indian Express":
+  //       return expressImg;
+  //     case "The Quint":
+  //       return quintImg;
+  //     case "Hindustan Times":
+  //       return hTimesImg;
+  //     case "Bollywood Life":
+  //       return bLifeImg;
+  //     case "The Times of India":
+  //       return toiImg;
+  //     case "123telugu.com":
+  //       return teluguImg;
+  //     case "PINKVILLA":
+  //       return pinkvillaImg;
+  //     case "BBC News":
+  //       return bbcImg;
+  //     case "News18":
+  //       return news18Img;
+  //     default:
+  //       return favImg;
+  //   }
+  // };
 
   return (
     <>
@@ -150,15 +151,15 @@ const NewsComponent = () => {
             {articles?.map((item) => (
               <Link to={`/news/${category}/${item.title}`} key={item.title} className=" relative hover:scale-105 duration-200 border bg-[#dfe3f1]  text-black rounded-lg w-[250px] h-[270px] ">
                 <div className=" absolute shadow-xl top-[47%] left-[40%] w-[40px] bg-white  p-[.4rem] rounded-full">
-                  <img className=" rounded-2xl" src={profileImg(item.source.name)} alt="" />
+                  <img className=" rounded-2xl" src={item.source_icon} alt="" />
                 </div>
                 <div className=" overflow-hidden p-1 h-[150px] z-10">
-                  <div className="  h-full w-full backImg rounded-md" style={{backgroundImage: `url(${item.urlToImage || defaultImg})`}}></div>
+                  <div className="  h-full w-full backImg rounded-md" style={{backgroundImage: `url(${item.image_url || defaultImg})`}}></div>
                 </div>
                 <div className="text-[.6rem] font-bold flex items-center justify-between px-1">
-                  <span className=" ">{item.source.name.toUpperCase().slice(0, 19)}</span>
+                  <span className=" ">{item.source_id.toUpperCase().slice(0, 19)}</span>
                   <span className=" ">
-                    {item.publishedAt.slice(0, 10).split("-").reverse().join("-")}, {item.publishedAt.slice(11, 16)}
+                    {item.pubDate.slice(0, 10).split("-").reverse().join("-")}, {item.pubDate.slice(11, 16)}
                   </span>
                 </div>
                 <div className="  h-[100px] px-2 flex  items-center justify-center text-center">
@@ -167,7 +168,8 @@ const NewsComponent = () => {
               </Link>
             ))}
           </div>
-          <div className=" bg-black   px-24 flex justify-between">
+
+          {/* <div className=" bg-black   px-24 flex justify-between">
             <button disabled={receivePage <= 1} onClick={() => dispatch(decrement())} className={` ${receivePage <= 1 ? "bg-red-400" : "bg-blue-400"} border rounded-md px-2 my-3`}>
               Previous
             </button>
@@ -178,7 +180,8 @@ const NewsComponent = () => {
             >
               Next
             </button>
-          </div>
+          </div> */}
+
         </div>
       )}
     </>
--- Super-App/server/index.js
+++ Super-App/server/index.js
@@ -1,33 +1,38 @@
-import mongoose from 'mongoose';
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import route from './routes/loginRoutes.js';
+import mongoose from "mongoose";
+import express from "express";
+// import cors from "cors";
+import dotenv from "dotenv";
+import route from "./routes/loginRoutes.js";
 
 const app = express();
-app.use(cors({
-    origin: 'https://super-app-demo.vercel.app'
-}));
+// app.use(
+//   cors({
+//     origin: "https://super-app-demo.vercel.app",
+//   })
+// );
 app.use(express.json());
 dotenv.config();
-app.use('/api', route); 
+app.use("/api", route);
 
 const PORT = process.env.PORT || 3000;
 const URI = process.env.MONGO_URI;
 
-mongoose.connect(URI).then(()=>{
-    console.log('successfully connected to MONGODB');
-    app.listen(PORT, ()=>{
-        console.log(`Running on port ${PORT}`);
-    })
-}).catch((err)=>{
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("successfully connected to MONGODB");
+    app.listen(PORT, () => {
+      console.log(`Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-app.use('/api', (req, res)=>{
-    res.send('hello api');
-})
+app.use("/api", (req, res) => {
+  res.send("hello api");
+});
 
-app.use('/', (req, res)=>{
-    res.send('hello user');
-})
+app.use("/", (req, res) => {
+  res.send("hello user");
+});
